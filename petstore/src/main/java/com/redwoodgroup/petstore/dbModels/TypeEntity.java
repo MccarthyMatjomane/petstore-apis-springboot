790ef159ea8ae3fe52970c5822f54cda80b77e70
MccarthyMatjomane <thagamccarthy@gmail.comPhogolo@8>
2021-09-02 19:34:45 +0200

    i added the remainig Entities with their Repository as well

---
+++ petstore/src/main/java/com/redwoodgroup/petstore/dbModels/TypeEntity.java
@@ -0,0 +1,40 @@
+package com.redwoodgroup.petstore.dbModels;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "type")
+public class TypeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator ="type_id_seq" )
+    @SequenceGenerator(name = "type_id_seq",sequenceName = "type_id_seq",allocationSize = 1)
+    private Integer id;
+    private String name;
+    @OneToMany(mappedBy = "type")
+    private List<PetEntity> pet;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<PetEntity> getPet() {
+        return pet;
+    }
+
+    public void setPet(List<PetEntity> pet) {
+        this.pet = pet;
+    }
+}
